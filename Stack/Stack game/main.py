d4fa2bd6a88f15b6064f67d4a271bcc4d6e06f28
LyleW473 <118493502+LyleW473@users.noreply.github.com>
2022-11-28 20:35:00 +0000

    Adding basic menu set-up (with temporary graphics) and an in-game timer.

---
+++ Stack/Stack game/main.py
@@ -0,0 +1,115 @@
+# Import modules
+import pygame, sys
+from pygame.locals import *
+from Menus import Menu
+from stack import Stack
+
+
+# Initialise pygame
+pygame.init()
+
+# Screen
+screen_width = 1000
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Colours
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+# Font
+time_font = pygame.font.SysFont("Bahnschrift", 150)
+
+# Game variables
+time_counter = 5000
+
+# Instances
+menu = Menu(0,0,screen)
+
+
+def draw_text(text, font, text_colour, x, y):
+    image = font.render(text, True, text_colour)
+    screen.blit(image, (x, y))
+
+
+def draw_alpha_text(text, font, text_colour,  x, y):
+    alpha_text = font.render(text, True ,text_colour)
+    alpha_text.set_alpha(70)
+    screen.blit(alpha_text,(x,y))
+
+
+# Main loop
+run = True
+while run:
+
+    
+
+    # Menu browsing and updating
+    if menu.in_game == False:
+        # Find the position of the mouse
+        pos = pygame.mouse.get_pos()
+        # Update the menu, feeding the clicked variable and mouse position into the function
+        menu.update(pos) # Set the clicked variable as the returned value from the menu
+
+        # Only if we are in the paused menu, should we draw a "faded" timer
+        if menu.show_paused_menu == True:
+            draw_alpha_text(str(round(time_counter / 1000, 2)), time_font, BLACK, 360, 0)
+
+
+    # INGAME
+    if menu.in_game == True:
+        screen.fill(BLUE)   
+
+        # Draw the timer at the top of the screen
+        draw_text(str(round(time_counter / 1000, 2)), time_font, BLACK, 360, 0)
+
+        # Constantly check the time
+        if pygame.time.get_ticks() - menu.entered_game_time > 1:
+            # The time should be (the time counter + any time that the player spent in the menu) - (the amount of time that its been since the last check)
+            time_counter = (time_counter + menu.menu_times_dictionary["in_menu_time"]) - (pygame.time.get_ticks() - menu.entered_game_time)
+
+            # Reset the values of all the menu times.
+            menu.menu_times_dictionary.update({"in_menu_time": 0, "entered_menu_time": 0})
+
+            # Set the current time to now so that we can keep checking the time
+            menu.entered_game_time = pygame.time.get_ticks()
+            
+            # If the time counter has gone below 0, reset it again
+            if time_counter <= 0:
+                time_counter = 5000
+
+
+        
+    
+
+    
+
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            run = False
+            pygame.quit()
+            sys.exit()
+
+        # Check if the mouse button has been pressed
+        if event.type == MOUSEBUTTONDOWN:
+            # Check if the mouse button clicked was the left click
+            if event.button == 1: # (1 = left, 2 = middle, 3 = right, 4 = scroll up, 5 = scrolldown)
+                menu.clicked = True
+
+        # Check if a key has been pressed
+        if event.type == KEYDOWN:
+            # Check if we are in game
+            if menu.in_game == True:    
+                # If the ESC key is pressed
+                if event.key == K_ESCAPE:
+                    # Show the paused menu
+                    menu.show_paused_menu = True
+                    menu.in_game = False
+
+
+
+    pygame.display.update()
---
+++ Stack/Stack game/stack.py
@@ -0,0 +1,39 @@
+class Stack():
+    def __init__(self, items_list = [] ):
+        self.item_count = len(items_list)
+        self.head_pointer = self.item_count - 1 # Set the head pointer (the pointer for the item at the top of the stack) (If it is empty, the pointer will be -1)
+        self.items_list = items_list # If a list isn't passed into the instance when first being instantiated, by default, it will be empty.
+
+    def push(self, item):
+        # Increment the head pointer 
+        self.head_pointer += 1
+        # Add the item onto the stack
+        self.items_list.append(item)
+
+    def pop(self):
+        # If the stack is empty, then don't do anything
+        if self.isEmpty() == True:
+            print("Unable to pop items when the stack is empty")
+        else:
+            print(f'{self.items_list[self.head_pointer]} has been popped off the stack')
+            # Pop the item off the stack
+            self.items_list.pop(self.head_pointer)
+            # Decrement the head pointer
+            self.head_pointer -= 1
+
+    def isEmpty(self):
+        # If the length of the list is greater than 0
+        if len(self.items_list) > 0:
+            # Then it isn't empty so return False
+            return False
+        # Otherwise return True
+        return True
+
+    def peek(self):
+        if self.isEmpty() == False:
+            print(f'The topmost element of the stack is {self.items_list[self.head_pointer]}')
+        else:
+            print("The stack is currently empty.")
+    
+    def size(self):
+        print(f'There are {len(self.items_list)} items in the stack')
