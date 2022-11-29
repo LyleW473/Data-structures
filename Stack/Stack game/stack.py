9883cf465c0809aba0d5178ad8fe2df0c90482ff
LyleW473 <118493502+LyleW473@users.noreply.github.com>
2022-11-29 00:02:47 +0000

    Fixing stack code

--- Stack/Stack game/stack.py
+++ Stack/Stack game/stack.py
@@ -1,5 +1,5 @@
 # Import modules
-import pygame, sys
+import pygame
 
 # Initialise pygame
 pygame.init()
@@ -9,16 +9,6 @@ screen_width = 1000
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Load images 
-# Most buttons are (400 x 25) pixels
-play_image = pygame.image.load('graphics/Buttons/play_button.png').convert()
-controls_image = pygame.image.load('graphics/Buttons/controls_button.png').convert()
-quit_image = pygame.image.load('graphics/Buttons/quit_button.png').convert()
-back_image = pygame.image.load('graphics/Buttons/back_button.png').convert()
-return_to_main_menu_image = pygame.image.load('graphics/Buttons/return_to_main_menu_button.png').convert()
-continue_image = pygame.image.load('graphics/Buttons/continue_button.png').convert()
-
-
 # Colours
 RED = (255,0,0)
 GREEN = (0,255,0)
@@ -26,179 +16,86 @@ BLUE = (0,0,255)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
+class Stack():
+    def __init__(self, items_list = [] ):
+        self.item_count = len(items_list)
+        self.items_list = items_list # If a list isn't passed into the instance when first being instantiated, by default, it will be empty.
+        for i in range(0, len(self.items_list)):
+            if self.items_list[i] == 2:
+                self.player_pointer = i
 
+    def travel_up(self):
+        # If we are traveling up the stack, we are going left in the list
+        
+        # If the player is already at the top of the stack
+        if self.player_pointer == 0:
+            # Do nothing
+            pass
+        # If the player pointer is any value other than the index at the top of the stack:
+        else:
+            # If the item isn't the goal element
+            if self.items_list[self.player_pointer - 1] != 1:
+                # Switch the player item and the item next to it around
+                self.items_list[self.player_pointer - 1], self.items_list[self.player_pointer]= self.items_list[self.player_pointer], self.items_list[self.player_pointer - 1]
 
-class Button():
-    def __init__(self, x, y, image):
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+            # If it is the goal element
+            else:
+                # Then set the item next to the player to be "overwritten"
+                self.items_list[self.player_pointer - 1] = self.items_list[self.player_pointer]
+                # Set the item that the player was just at to be nothing
+                self.items_list[self.player_pointer] = 0
 
-    def update(self, pos):
-        mouse_over_button = False
-        # Check for a collision between the button and the current mouse position
-        if self.rect.collidepoint(pos):
-            mouse_over_button = True
+            # --------------------------------- ADD CODE HERE FOR GENERATING A NEW GOAL ELEMENT
+              
+            # Decrement the player pointer
+            self.player_pointer -= 1
+        
 
-        # Draw the button
-        screen.blit(self.image, self.rect)
+    def travel_down(self):
+        # If we are traveling down the stack, we are going right in the list
 
-        # Return the clicked variable to the menu
-        return mouse_over_button
+        # If the player pointer is at the bottom of the stack already
+        if self.player_pointer == len(self.items_list) -1:
+            # Do nothing
+            pass
+        else:
 
+            # If the item isn't the goal element
+            if self.items_list[self.player_pointer + 1] != 1:
+                # Switch the player item and the item next to it around
+                self.items_list[self.player_pointer + 1], self.items_list[self.player_pointer]= self.items_list[self.player_pointer], self.items_list[self.player_pointer + 1]
 
+            # If it is the goal element
+            else:
+                # Then set the item next to the player to be "overwritten"
+                self.items_list[self.player_pointer + 1] = self.items_list[self.player_pointer]
+                # Set the item that the player was just at to be nothing
+                self.items_list[self.player_pointer] = 0
 
-class Menu():
-    def __init__(self, x, y, surface):
-        # Surface that the menu will be drawn onto
-        self.surface = surface
-        # Buttons
-        self.button_list = []
-        self.clicked = False # Used to track whenever the buttons on the menus are clicked
+                # --------------------------------- ADD CODE HERE FOR GENERATING A NEW GOAL ELEMENT
 
-        # Game states
-        self.show_main_menu = True # Determines whether we show the main menu or not
-        self.show_controls_menu = False
-        self.in_game = False # Determines whether we are in game or not
-        self.show_paused_menu = False
+            # Increment the player pointer
+            self.player_pointer += 1
 
-        self.last_menu_visited = 0 # 1 = Main menu, 2 = Paused menu
+    def update_stack(self):
+        pass
 
-        # Time tracking
-        self.menu_times_dictionary = {"in_menu_time": 0, "entered_menu_time": 0}
+    def draw(self):
+        # Width and height for the stack element rectangles
+        rect_width = 200
+        rect_height = 75
+        
+        # Generate rectangles (one for each stack element)
+        for i in range(0, len(self.items_list)):
+            # Any other stack item 
+            if self.items_list[i] == 0:
+                stack_item_colour = WHITE
+            # The goal position
+            elif self.items_list[i] == 1:
+                stack_item_colour = GREEN
+            # The player 
+            elif self.items_list[i] == 2:
+                stack_item_colour = RED
 
-    def update(self, pos):
-
-        # MAIN MENU
-        if self.show_main_menu == True:
-            # Fill the screen with a white background
-            screen.fill(WHITE)
-
-            # PLAY BUTTON
-            # If the mouse is over the play button and is the mouse button is clicked
-            if play_button.update(pos) == True and self.clicked == True: 
-                # Reset the clicked variable to default so more clicks can be detected
-                self.clicked = False
-                # Set the time that the player entered the game to be now
-                self.entered_game_time = pygame.time.get_ticks()
-                # Set the game already started variable to True
-                self.game_already_started = True
-                # Set the main menu to stop showing and start the game
-                self.show_main_menu = False
-                self.in_game = True
-
-            # CONTROLS BUTTON
-            if controls_button.update(pos) == True and self.clicked == True: 
-                # Reset the clicked variable to default so more clicks can be detected
-                self.clicked = False
-
-                # Display the show controls menu
-                self.show_controls_menu = True
-                self.show_main_menu = False
-
-                # Set the last menu visited from the controls menu to be the paused menu
-                self.last_menu_visited = 1
-
-            # QUIT BUTTON
-            # If the mouse is over the quit button and is the mouse button is clicked
-            if quit_button.update(pos) == True and self.clicked == True:
-                # Quit the game
-                pygame.quit()
-                sys.exit()
-
-            # If none of the buttons above are True, that means the player clicked on empty space
-            elif play_button.update(pos) == False and quit_button.update(pos) == False and self.clicked == True: 
-                # Reset the clicked variable to default so more clicks can be detected
-                self.clicked = False
-
-            
-        # CONTROLS MENU
-        if self.show_controls_menu == True:
-            screen.fill(GREEN)
-
-            # BACK BUTTON
-            if back_button.update(pos) == True and self.clicked == True:
-                # Reset the clicked variable to default so more clicks can be detected
-                self.clicked = False
-
-                # Go back to the last menu 
-                if self.last_menu_visited == 1: # MAIN MENU
-                    self.show_main_menu = True
-
-                elif self.last_menu_visited == 2: # PAUSED MENU
-                    self.show_paused_menu = True
-
-                # Don't show the controls menu
-                self.show_controls_menu = False
-
-            # If none of the buttons above are True, that means the player clicked on empty space
-            elif back_button.update(pos) == False and self.clicked == True: 
-                # Reset the clicked variable to default so more clicks can be detected
-                self.clicked = False
-
-
-        # PAUSED MENU
-        if self.show_paused_menu == True:
-            screen.fill(RED)
-
-            # Check if the value inside "entered menu time" key in the menu times dictionary is 0
-            if self.menu_times_dictionary["entered_menu_time"] == 0:
-                # If it is, this means that the player has entered the paused menu, so record the current time.
-                self.menu_times_dictionary["entered_menu_time"] = pygame.time.get_ticks()
-            
-            # CONTINUE BUTTON
-            if continue_button.update(pos) == True and self.clicked == True: 
-                # Reset the clicked variable to default so more clicks can be detected
-                self.clicked = False
-
-                # Calculate the time that the player was in the menu
-                self.menu_times_dictionary["in_menu_time"] = pygame.time.get_ticks() - self.menu_times_dictionary["entered_menu_time"] 
-
-                # Go back to the main game
-                self.in_game = True
-                self.show_paused_menu = False
-
-            # CONTROLS BUTTON
-            if controls_button_2.update(pos) == True and self.clicked == True: 
-                # Reset the clicked variable to default so more clicks can be detected
-                self.clicked = False
-
-                # Display the show controls menu
-                self.show_controls_menu = True
-                self.show_paused_menu = False   
-                
-                # Set the last menu visited from the controls menu to be the paused menu
-                self.last_menu_visited = 2
-
-            # QUIT BUTTON
-            # If the mouse is over the quit button and is the mouse button is clicked
-            if quit_button_2.update(pos) == True and self.clicked == True:
-                # Quit the game
-                pygame.quit()
-                sys.exit()
-
-            # If none of the buttons above are True, that means the player clicked on empty space
-            elif continue_button.update(pos) == False and controls_button_2.update(pos) == False and quit_button_2.update(pos) == False and self.clicked == True:
-                # Reset the clicked variable to default so more clicks can be detected
-                self.clicked = False             
-      
-
-
-
-# Button instances
-
-# Main menu
-play_button = Button(300, 200 , play_image)
-controls_button = Button(300, 400, controls_image)
-quit_button = Button(300, 600, quit_image)
-
-# Controls menu
-back_button = Button(300, 600, back_image)
-return_to_main_menu_button = Button(300, 400, return_to_main_menu_image)
-
-
-# Paused menu
-continue_button = Button(300, 200, continue_image)
-controls_button_2 = Button(300, 400, controls_image)
-quit_button_2 = Button(300, 600, quit_image)
+            # Draw the rectangle
+            pygame.draw.rect(screen, stack_item_colour, (500 - (rect_width / 2), 200 + (i * (rect_height + 5)), rect_width, rect_height), 0)
