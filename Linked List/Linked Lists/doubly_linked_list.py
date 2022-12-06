72b2ba8fbb88147bf557e04369f9ba8c1594126f
Lyle Wong <lylereturns@gmail.com>
2022-12-06 21:18:17 +0000

    Method created for DLL: - To add a node to the end of the DLL. - If the DLL is empty, the added node becomes the head and tail node.

--- Linked List/Linked Lists/doubly_linked_list.py
+++ Linked List/Linked Lists/doubly_linked_list.py
@@ -11,6 +11,7 @@ class DoublyLinkedList:
 
     def add_to_head(self, new_head_node):
         old_head_node = self.head_node
+        
         # If there already is an existing head node
         if self.head_node != None:
             # Set the new head node's next node to be the old head node
@@ -29,6 +30,28 @@ class DoublyLinkedList:
             # The new node will also be the new tail node (if there was nothing before)
             self.tail_node = new_head_node
         
+    def add_to_tail(self, new_tail_node):
+        old_tail_node = self.tail_node
+
+        # If there already is an existing tail node
+        if self.tail_node != None:
+            # Set the new tail's next node to be Nothing
+            new_tail_node.next_node = None
+            # Set the new tail's previous node to be the old tail node
+            new_tail_node.prev_node = old_tail_node
+            # Set the old tail node's next node to be the new tail
+            old_tail_node.next_node = new_tail_node
+            # Set the new tail node to be the tail node
+            self.tail_node = new_tail_node
+
+        # If there is no tail node
+        if self.tail_node == None:
+            # Set the node passed into the function as the new tail node
+            self.tail_node = new_tail_node
+            # The new node will also be the new head node (if there was nothing before)
+            self.head_node = new_tail_node
+
+
     def output(self):
         current_node = self.head_node # Node used to iterate through the LL
         list_of_items = [] # List to hold the values of all the nodes in the LL
@@ -49,4 +72,7 @@ my_dll.add_to_head(Node(32))
 my_dll.add_to_head(Node(100))
 my_dll.add_to_head(Node(23))
 my_dll.output()
+my_dll.add_to_tail(Node(62))
+my_dll.output()
+
 
