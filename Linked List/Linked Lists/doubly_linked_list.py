9f2d5b48204ecda3168462265ef13015a74958b3
Lyle Wong <lylereturns@gmail.com>
2022-12-06 22:00:18 +0000

    Method created to remove a node from the DLL: - Simultaneously traverses the DLL from both sides. - Check added in case that the node passed in is not in the DLL. - Checks added in case that the node is the head/tail node. - Nodes in between the tail node and head node can be removed.

--- Linked List/Linked Lists/doubly_linked_list.py
+++ Linked List/Linked Lists/doubly_linked_list.py
@@ -11,7 +11,7 @@ class DoublyLinkedList:
 
     def add_to_head(self, new_head_node):
         old_head_node = self.head_node
-        
+
         # If there already is an existing head node
         if self.head_node != None:
             # Set the new head node's next node to be the old head node
@@ -51,11 +51,86 @@ class DoublyLinkedList:
             # The new node will also be the new head node (if there was nothing before)
             self.head_node = new_tail_node
 
+    def remove_node(self, node_value_to_remove):
+        start_current_node = self.head_node # Tracks the current node we are from the start of the list
+        rear_current_node = self.tail_node # Tracks the current node we are from the end of the list
+        node_to_remove = None # Will be set to the node to remove when found
 
+        # FINDING THE NODE TO REMOVE
+        
+        # If the value of the current node at the start of the list is the same as the value of the node we want to remove
+        if start_current_node.node_val == node_value_to_remove:
+            # Set the node to remove as the head node
+            node_to_remove = self.head_node
+
+
+        # If the value of the current node at the rear of the list is the same as the value of the node we want to remove
+        if rear_current_node.node_val == node_value_to_remove:
+            # Set the node to remove as the tail node
+            node_to_remove = self.tail_node
+
+        # Move the current nodes inwards (As they cannot be the head node or tail node)
+        start_current_node = start_current_node.next_node
+        rear_current_node = rear_current_node.prev_node
+        
+        # While we haven't found the node to remove
+        while node_to_remove == None:
+
+            # If the rear current node has reached the start of the list and the start current node reached the end of the list
+            if rear_current_node == self.head_node and start_current_node == self.tail_node:
+                print("End of list reached, node not found!")
+                break
+            
+            # Check if the value of the start current node is the same as the value of the node we want to remove
+            if start_current_node.node_val == node_value_to_remove:
+                # Set the node to remove to be the current node
+                node_to_remove = start_current_node
+
+            # Check if the value of the rear current node is the same as the value of the node we want to remove
+            if rear_current_node.node_val == node_value_to_remove:
+                # Set the node to remove to be the current node
+                node_to_remove = rear_current_node
+
+            # Increment the start current node (moves inwards towards the rear node)
+            start_current_node = start_current_node.next_node
+            # Decrement the start current node (moves inwards towards the head node)
+            rear_current_node = rear_current_node.prev_node
+
+        # ----------------------------------------------------------------------------------
+        # CHANGING THE POINTERS
+
+        # In the case that the node was not found inside the DLL
+        if node_to_remove == None:
+            # Return nothing
+            return None
+
+        # If the node to remove is the tail 
+        elif node_to_remove == self.tail_node:
+            print("remove_tail")
+            #self.remove_tail()
+            
+        
+        elif node_to_remove == self.head_node:
+            print("remove head")
+            #self.remove_head()
+
+        # Otherwise if the node to remove is in between the head node and the tail node 
+        else:
+            # Set the next node pointer of the node before the node we want to remove to point to what the node we want to remove is pointing to
+            node_to_remove.prev_node.next_node = node_to_remove.next_node
+
+            # Set the prev node pointer of the node after the node we want to remove to point to the previous node of the node we want to remove
+            node_to_remove.next_node.prev_node = node_to_remove.prev_node
+
+        # Output the current state of the list
+        self.output()
+
+            
     def output(self):
-        current_node = self.head_node # Node used to iterate through the LL
-        list_of_items = [] # List to hold the values of all the nodes in the LL
+        current_node = self.head_node # Node used to iterate through the DLL
+        list_of_items = [] # List to hold the values of all the nodes in the DLL
 
+        # While we haven't reached the end of the DLL
         while current_node != None:
             # Append the value of the current node
             list_of_items.append(current_node.node_val)
@@ -75,4 +150,4 @@ my_dll.output()
 my_dll.add_to_tail(Node(62))
 my_dll.output()
 
-
+my_dll.remove_node(97)
