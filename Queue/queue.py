a1418637e8dcd79ff9cd1b3650d6d3efdfd6ee5a
Lyle Wong <lylereturns@gmail.com>
2022-12-08 17:25:29 +0000

    Added functionality to Queue methods: - Enqueue method is complete. - Method created to output all the values of the items inside the queue. - Added "next_node" attribute to the Node class.

--- Queue/queue.py
+++ Queue/queue.py
@@ -1,6 +1,7 @@
 class Node:
-    def __init__(self, node_val):
+    def __init__(self, node_val, next_node = None):
         self.node_val = node_val
+        self.next_node = next_node
 
 class Queue:
     def __init__(self, max_size = None):
@@ -9,13 +10,34 @@ class Queue:
         self.rear = None # Initially has no value as there are no items in the queue
         self.max_size = max_size
 
-    def enqueue(self, new_item):
+    def enqueue(self, new_item_value):
         # If the queue has no declared max size, or the size of the queue is not max_size.
         if self.isFull() == False:
-            pass
+            # Create a node with the new item value
+            new_item = Node(new_item_value)
+
+            # If the queue is empty
+            if self.isEmpty() == True:
+                # The new node will become the head and rear of the queue  
+                self.head, self.rear = new_item, new_item
+
+            # If the queue is not empty
+            if self.isEmpty() == False:
+                # Set the old rear node's next node to be the new item
+                self.rear.next_node = new_item
+                # Set the rear node as the new item
+                self.rear = new_item
+
+            # Increment the size of the queue
+            self.size += 1 
+
+            # Print all of the items inside the queue
+            self.output()
+
         # Otherwise, if the queue is full
         else:
             print("Cannot enqueue to a full list")
+
 
     def dequeue(self):
         pass
@@ -46,6 +68,22 @@ class Queue:
         # Return the size of the queue
         return self.size
 
+    def output(self):
+        current_node = self.head # Start at the start of the queue
+        output_list = [] # List to hold the values of each item in the queue
 
-myQueue = Queue()
-myQueue.enqueue("hehre")
+        while current_node: # Same as "while current_node != None"
+            # Append the current node's value to the output list
+            output_list.append(current_node.node_val)
+            # Go to the next node
+            current_node = current_node.next_node
+
+        # Print the output list
+        print(output_list)
+
+
+myQueue = Queue(3)
+myQueue.enqueue("hello")
+myQueue.enqueue(23)
+myQueue.enqueue("world")
+myQueue.enqueue(53)
