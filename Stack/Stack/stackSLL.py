3ad6c2be1c776fcfa3c4cc8c7d1dfbe15f713316
Lyle Wong <lylereturns@gmail.com>
2022-12-08 19:26:01 +0000

    Creating a stackSLL file (Singly Linked List): - Created the Stack and Node classes. - Listed all of the methods and attributes the Stack class need.

---
+++ Stack/Stack/stackSLL.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, node_val, next_node = None):
+        self.node_val = node_val
+        self.next_node = next_node
+
+class Stack:
+    def __init__(self, max_size = 10):
+        self.max_size = max_size
+        self.top_item = None
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def isFull(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def output(self):
+        pass
+
